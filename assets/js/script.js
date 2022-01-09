38b7306e11cdae94e11f954d0da7eb443a1365f6
hjohnsick <hjohnsick@yahoo.com>
2022-01-09 15:33:56 -0500

    fixed city display

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,9 @@ var getWeatherDataByCity = function(city) {
             response.json().then(function(data) {
                 var lat = data.city.coord.lat;
                 var lon = data.city.coord.lon;
+                var city = data.city.name;
                 console.log(data);
-                getForecastByLatLon(lat, lon);
+                getForecastByLatLon(lat, lon, city);
                 
             });
         }
@@ -25,7 +26,7 @@ var getWeatherDataByCity = function(city) {
 }
 
 // Get weather for current day
-var getForecastByLatLon = function(lat, lon) {
+var getForecastByLatLon = function(lat, lon, city) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${key}`;
 
     fetch(apiUrl).then(function(response) {
@@ -36,8 +37,6 @@ var getForecastByLatLon = function(lat, lon) {
                 var currentTemp = data.current.temp;
                 var humidity = data.current.humidity;
                 var windSpeed = data.current.wind_speed;
-                var timezone = data.timezone;
-                var city = timezone.split('/')[1];
                 var timestamp = data.current.dt;
                 var dateTime = new Date(timestamp*1000).toLocaleString();
                 var date = moment(new Date(dateTime)).format('L');
@@ -91,11 +90,36 @@ $(".btn").on("click", formSubmitHandler);
 var todaysForecast = function(cityName, date, icon, temp, wind, humidity, uvIndex) {
     $("#todays-forecast").append(`<h3>${cityName} ${date} <img src="http://openweathermap.org/img/w/${icon}.png" alt="current weather"></h3>`);
     $("#todays-forecast").append(`<ul class="weather-data"><li>Temp: ${temp} &#8457</li><li>Wind: ${wind} MPH</li><li>Humidity: ${humidity}%</li></ul>`).css("border", "2px solid #40C3E0");
-    $(".weather-data").append(`<li>UV Index: ${uvIndex}`);
+    $(".weather-data").append(`<li id="uv-index">UV Index: <span>${uvIndex}</span>`);
+    changeColorOfUVIndex(uvIndex);
 }
 
 // display 5 day forecast
 var fiveDayForecast = function(index, date, icon, temp, wind, humidity) {
     $(`#day-${index}`).append(`<ul class="day"><li>${date}</li><li><img src="http://openweathermap.org/img/w/${icon}.png" alt="current weather"></li><li>Temp: ${temp} &#8457</li><li>Wind: ${wind} MPH</li><li>Humidity: ${humidity}%</li></ul>`);
     $('.day').css({ 'background-color': '#40C3E0', 'padding': '4px'});
+}
+
+var changeColorOfUVIndex = function (uvIndex) {
+    if (uvIndex <= 2) {
+        // change background color to green
+        $("span").css({ 'background-color': '#228C22', 'color': '#FFFFFF', 'padding-left': '5px', 'padding-right': '5px' });
+        console.log("green");
+    } else if (uvIndex <= 6) {
+        // change background color to yellow
+        $("span").css({ 'background-color': '#FFFF00', 'color': '#000000', 'padding-left': '5px', 'padding-right': '5px' });
+        console.log("yellow");
+    } else if (uvIndex <= 7) {
+        // change background color to orange
+        $("span").css({ 'background-color': '#FF6700', 'color': '#FFFFFF', 'padding-left': '5px', 'padding-right': '5px' });
+        console.log("orange");
+    } else if (uvIndex <= 10 ) {
+        // change background color to red
+        $("span").css({ 'background-color': '#FF0000', 'color': '#FFFFFF', 'padding-left': '5px', 'padding-right': '5px' });
+        console.log("red");
+    } else {
+        // change background color to purple
+        $("span").css({ 'background-color': '#551A8B', 'color': '#FFFFFF', 'padding-left': '5px', 'padding-right': '5px' });
+        console.log("purple");
+    }
 }
